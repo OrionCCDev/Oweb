e856750611bc8ed01b3cfd1e71e3e57e29e58c5e
OrionCCDev <developer@orioncc.com>
2025-02-02 15:26:20 +0400

    v3

--- database/migrations/2024_10_02_093632_create_projects_table.php
+++ database/migrations/2024_10_02_093632_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('slug_name')->nullable();
+            $table->string('sub_name')->nullable();
             $table->enum('status' ,['completed' , 'in progress']);
             $table->enum('priority' ,['h-v1' , 'h-v2' , 'm-v1' , 'm-v2' , 'l-v1' , 'l-v2']);
             $table->string('main_image')->default('main.webp');
